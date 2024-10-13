d9911d8a24daaaf0dd1fda99cd7ede24f0df0f34
Simon Coles <simonc@amphora-research.com>
2024-10-13 12:18:17 +0100

    Redirects, again

--- functions/bc/[id].js
+++ functions/bc/[id].js
@@ -1,3 +1,4 @@
+// This is a Cloudflare Pages Function to redirect from the /bc/ paths here to the /r/bc- paths on bettercourses.org
 export async function onRequest(context) {
     const id = context.params.id;
     const redirectUrl = `https://bettercourses.org/r/bc-${id}`;
